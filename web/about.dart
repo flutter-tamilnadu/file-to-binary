d4ffefac6ad54d4e49e52da6eaa4451ce0947c82
yogireddy <yogireddy02@gmail.com>
2025-06-27 00:08:16 +0530

    base64 completed

---
+++ web/about.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:web/web.dart' as web;
+
+class AboutWidget extends StatelessWidget {
+  const AboutWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        web.window.location.href = '/about.html';
+      },
+      child: Icon(Icons.account_circle_rounded,
+          color: Colors.blueAccent,
+          size: 20),
+    );
+  }
+}
